ec973c13ec38112fc57b1352bca61b438b35b0ed
JonahPeck <PeckJonahC@gmail.com>
2024-07-30 13:58:33 -0400

    finished cli with basic functionality

--- rptodo_project/rptodo/rptodo.py
+++ rptodo_project/rptodo/rptodo.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 from typing import Any, Dict, List, NamedTuple
 
-from rptodo import DB_READ_ERROR
+from rptodo import DB_READ_ERROR, ID_ERROR
 from rptodo.database import DatabaseHandler
 
 class CurrentTodo(NamedTuple):
@@ -13,6 +13,24 @@ class CurrentTodo(NamedTuple):
 class Todoer:
     def __init__(self, db_path: Path) -> None:
         self._db_handler = DatabaseHandler(db_path)
+
+    def remove_all(self) -> CurrentTodo:
+        """Remove all to-dos from the database."""
+        write = self._db_handler.write_todos([])
+        return CurrentTodo({}, write.error)
+
+    def set_done(self, todo_id: int) -> CurrentTodo:
+        """Set a to-do as done."""
+        read = self._db_handler.read_todos()
+        if read.error:
+            return CurrentTodo({}, read.error)
+        try:
+            todo = read.todo_list[todo_id - 1 ]
+        except IndexError:
+            return CurrentTodo({}, ID_ERROR)
+        todo["Done"] = True
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
     
     def get_todo_list(self) -> List[Dict[str, Any]]:
         """Return the current to-do list"""
@@ -33,5 +51,17 @@ class Todoer:
         if read.error == DB_READ_ERROR:
             return CurrentTodo(todo, read.error)
         read.todo_list.append(todo)
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
+
+    def remove(self, todo_id: int) -> CurrentTodo:
+        """Remove a to-do from the database using its id or index"""
+        read = self._db_handler.read_todos()
+        if read.error:
+            return CurrentTodo({}, read.error)
+        try:
+            todo = read.todo_list.pop(todo_id - 1)
+        except IndexError:
+            return CurrentTodo({}, ID_ERROR)
         write = self._db_handler.write_todos(read.todo_list)
         return CurrentTodo(todo, write.error)
